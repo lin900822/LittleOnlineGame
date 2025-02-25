87a76a05415e4bb8fff3e17310ea0fb1d0718993
lin <wilson24386433@gmail.com>
2025-02-26 04:01:46 +0800

    feat: 遊戲核心邏輯

--- Assets/Scripts/Game/MonoBehaviours/SnakeUnit.cs
+++ Assets/Scripts/Game/MonoBehaviours/SnakeUnit.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Game.MonoBehaviours
 {
@@ -8,15 +10,64 @@ namespace Game.MonoBehaviours
         private Vector2 _lastPos;
         private Vector2 _targetPos;
 
-        private float _startTime; 
+        private float _startTime;
         private const float LERP_DURATION = 1f / 20f; // 每幀 0.05 秒
 
-        public void UpdatePos(int x, int y)
+        private Transform _snakeUnitBodyPrefab;
+        private List<Transform> _snakeUnitBodies;
+
+        private List<Vector2> _lastPoses;
+        
+        private List<Vector2> Body { get; set; }
+
+        private void Awake()
         {
+            _snakeUnitBodyPrefab = Resources.Load<Transform>("Prefabs/SnakeUnit_Body");
+        }
+
+        public void UpdateStatus(int x, int y, int length)
+        {
+            if (Body == null)
+            {
+                Body = new List<Vector2>();
+                _snakeUnitBodies = new List<Transform>();
+                _lastPoses = new List<Vector2>();
+
+                for (int i = 0; i < length; i++)
+                {
+                    Body.Add(_targetPos);
+                    _lastPoses.Add(_targetPos);
+                    var obj = Instantiate(_snakeUnitBodyPrefab,_targetPos, Quaternion.identity, transform);
+                    _snakeUnitBodies.Add(obj);
+                }
+            }
+            else
+            {
+                for (int i = Body.Count; i < length; i++)
+                {
+                    Body.Add(Body[^1]);
+                    _lastPoses.Add(Body[^1]);
+                    var obj = Instantiate(_snakeUnitBodyPrefab,_targetPos, Quaternion.identity, transform);
+                    _snakeUnitBodies.Add(obj);
+                }
+            }
+            
+            for (var i = 0; i < _snakeUnitBodies.Count; i++)
+            {
+                _lastPoses[i] = _snakeUnitBodies[i].position;
+            }
+            
+            for (int i = Body.Count - 1; i > 0; i--)
+            {
+                Body[i] = Body[i - 1];
+            }
+            
             _lastPos = _targetPos;
             _targetPos = new Vector2(x / 1000f, y / 1000f);
-        
+
             _startTime = Time.time; // 記錄開始時間
+            
+            Body[0] = _targetPos;
         }
 
         private void Update()
@@ -25,6 +76,11 @@ namespace Game.MonoBehaviours
             float lerpRatio = Mathf.Clamp01(elapsedTime / LERP_DURATION);
 
             transform.position = Vector2.Lerp(_lastPos, _targetPos, lerpRatio);
+            
+            for (var i = 0; i < _snakeUnitBodies.Count; i++)
+            {
+                _snakeUnitBodies[i].position = Vector2.Lerp(_lastPoses[i], Body[i], lerpRatio);
+            }
 
             // 確保補間完成時精確到目標點
             if (lerpRatio >= 1)
--- Assets/Scripts/Game/Services/BattleService.cs
+++ Assets/Scripts/Game/Services/BattleService.cs
@@ -4,6 +4,7 @@ using Shared;
 using Shared.Common;
 using Shared.Logger;
 using Shared.Network;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Game
@@ -14,19 +15,26 @@ namespace Game
 
         private GameObject _battleGO;
         private SnakeUnit _snakeUnitPrefab;
-        
+
+        private GameObject _food;
+        private GameObject _foodPrefab;
+
         private SnakeUnit _player1SnakeUnit;
         private SnakeUnit _player2SnakeUnit;
 
         private ByteBuffer _inputByteBuffer = new ByteBuffer();
 
         private bool _isBattleStart;
+        
+        private Vector2 startPos;
+        private bool isDragging = false;
 
         public override void Init()
         {
             _networkSystem.RegisterMessageHandler(MessageId.M2C_RoomMatched, M2C_RoomMatched);
-            
+
             _snakeUnitPrefab = Resources.Load<SnakeUnit>($"Prefabs/SnakeUnit");
+            _foodPrefab = Resources.Load<GameObject>($"Prefabs/Food");
 
             Log.Info("BattleService Init");
         }
@@ -62,6 +70,35 @@ namespace Game
                 _inputByteBuffer.WriteInt32(0);
                 _battleAgent.SendMessage((ushort)MessageId.C2B_Input, _inputByteBuffer);
             }
+            
+            // 按下滑鼠左鍵
+            if (Input.GetMouseButtonDown(0))
+            {
+                startPos = Input.mousePosition;
+                isDragging = true;
+            }
+
+            // 拖曳過程中計算角度
+            if (isDragging && Input.GetMouseButton(0))
+            {
+                Vector2 currentPos = Input.mousePosition;
+                int angle = (int)CalculateAngle(startPos, currentPos);
+                _inputByteBuffer.WriteInt32(angle);
+                _battleAgent.SendMessage((ushort)MessageId.C2B_Input, _inputByteBuffer);
+                Debug.Log($"當前角度: {angle:F2}°");
+            }
+
+            // 放開滑鼠左鍵
+            if (Input.GetMouseButtonUp(0))
+            {
+                isDragging = false;
+            }
+        }
+        
+        private float CalculateAngle(Vector2 start, Vector2 end)
+        {
+            Vector2 direction = end - start;
+            return Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         }
 
         private void M2C_RoomMatched(NetworkCommunicator communicator, ByteBuffer byteBuffer)
@@ -91,7 +128,7 @@ namespace Game
             _battleAgent.RegisterMessageHandler((ushort)MessageId.B2C_BattleStart, B2C_BattleStart);
             _battleAgent.RegisterMessageHandler((ushort)MessageId.B2C_SyncState, B2C_SyncState);
             _battleAgent.RegisterMessageHandler((ushort)MessageId.B2C_BattleEnd, B2C_BattleEnd);
-            
+
             var c2BJoinRoom = new C2B_JoinRoom()
             {
                 KeyToEnterRoom = keyToEnterRoom,
@@ -108,35 +145,51 @@ namespace Game
 
             _uIManager.HideAllWindows();
 
-            var player1X = byteBuffer.ReadInt32();
-            var player1Y = byteBuffer.ReadInt32(); 
-            var player2X = byteBuffer.ReadInt32();
-            var player2Y = byteBuffer.ReadInt32();
-
             _battleGO = GamePlayApp.Instance.BattleGO;
             _battleGO.SetActive(true);
+
+            _food = Object.Instantiate(_foodPrefab);
+
+            var player1X = byteBuffer.ReadInt32();
+            var player1Y = byteBuffer.ReadInt32();
+            var player1Length = byteBuffer.ReadInt32();
+            var player2X = byteBuffer.ReadInt32();
+            var player2Y = byteBuffer.ReadInt32();
+            var player2Length = byteBuffer.ReadInt32();
+            var foodX = byteBuffer.ReadInt32();
+            var foodY = byteBuffer.ReadInt32();
             _player1SnakeUnit = Object.Instantiate(_snakeUnitPrefab, _battleGO.transform);
-            _player1SnakeUnit.UpdatePos(player1X, player1Y);
+            _player1SnakeUnit.UpdateStatus(player1X, player1Y, player1Length);
             _player2SnakeUnit = Object.Instantiate(_snakeUnitPrefab, _battleGO.transform);
-            _player2SnakeUnit.UpdatePos(player2X, player2Y);
+            _player2SnakeUnit.UpdateStatus(player2X, player2Y, player2Length);
+            _food.transform.position = new Vector2(foodX / 1000f, foodY / 1000f);
         }
 
         private void B2C_SyncState(NetworkCommunicator communicator, ByteBuffer byteBuffer)
         {
             var player1X = byteBuffer.ReadInt32();
-            var player1Y = byteBuffer.ReadInt32(); 
+            var player1Y = byteBuffer.ReadInt32();
+            var player1Length = byteBuffer.ReadInt32();
             var player2X = byteBuffer.ReadInt32();
             var player2Y = byteBuffer.ReadInt32();
+            var player2Length = byteBuffer.ReadInt32();
+            var foodX = byteBuffer.ReadInt32();
+            var foodY = byteBuffer.ReadInt32();
+            _player1SnakeUnit.UpdateStatus(player1X, player1Y, player1Length);
+            _player2SnakeUnit.UpdateStatus(player2X, player2Y, player2Length);
 
-            _player1SnakeUnit.UpdatePos(player1X, player1Y);
-            _player2SnakeUnit.UpdatePos(player2X, player2Y);
+            _food.transform.position = new Vector2(foodX / 1000f, foodY / 1000f);
         }
-        
+
         private void B2C_BattleEnd(NetworkCommunicator communicator, ByteBuffer byteBuffer)
         {
-            Object.Destroy(_player1SnakeUnit.gameObject);
-            Object.Destroy(_player2SnakeUnit.gameObject);
-            
+            for (int i = 0; i < 5; i++)
+            {
+                Object.Destroy(_player1SnakeUnit.gameObject);
+                Object.Destroy(_player2SnakeUnit.gameObject);
+                Object.Destroy(_food.gameObject);
+            }
+
             _battleGO = GamePlayApp.Instance.BattleGO;
             _battleGO.SetActive(false);
 
